b51ac4903eb0cb66239edb9f4e16ef8e427febe1
MaksymNanyzniuk <nanymaks12@gmail.com>
2021-03-04 11:39:42 +0200

    Settings page tests (#5)
    * Added 3 tests for buttons: Save, Cancel, Logout
    
    * Added test Date_Format_Applied
    
    * Added test Number_Format_Applied
    
    * Added test Currency_Applied

--- web.test.tests/web.test.tests/SettingsPageTests.cs
+++ web.test.tests/web.test.tests/SettingsPageTests.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace web.test.tests
@@ -112,6 +113,108 @@ namespace web.test.tests
 
             //Assert
             Assert.AreEqual(exp_end_date, act_end_date);
+        }
+
+
+        [Test]
+        public void Number_Format_Applied()
+        {
+            //Arrange
+            int number_format_item = 0; //[0; 3]
+           
+            Decimal amount = 12345m;
+            int rate = 74;
+            int term = 211;
+
+            String financial_year_Id = "d365";
+            int year_length = 365;
+
+            //Act
+            SelectElement number_format_select = new SelectElement(driver.FindElement(By.XPath("//tr[2]/td/select")));
+            number_format_select.SelectByIndex(number_format_item);
+            
+            String selected_number_format_setting = driver.FindElement(By.XPath("//tr[2]/td/select")).GetAttribute("value");
+            
+            driver.FindElement(By.Id("save")).Click();
+            new WebDriverWait(driver, TimeSpan.FromMilliseconds(10000)).Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("amount")));
+
+            driver.FindElement(By.Id("amount")).Clear();
+            driver.FindElement(By.Id("amount")).SendKeys(amount.ToString());
+
+            driver.FindElement(By.Id("percent")).Clear();
+            driver.FindElement(By.Id("percent")).SendKeys(rate.ToString());
+
+            driver.FindElement(By.Id("term")).Clear();
+            driver.FindElement(By.Id("term")).SendKeys(term.ToString());
+
+            driver.FindElement(By.Id(financial_year_Id)).Click();
+
+            NumberFormatInfo nfi = new CultureInfo("en-US", true).NumberFormat;
+            switch (selected_number_format_setting)
+            {
+                case "123,456,789.00":
+                    break;
+                case "123.456.789,00":
+                    nfi.NumberGroupSeparator = ".";
+                    nfi.NumberDecimalSeparator = ",";
+                    break;
+                case "123 456 789.00":
+                    nfi.NumberGroupSeparator = " ";
+                    break;
+                case "123 456 789,00":
+                    nfi.NumberGroupSeparator = " ";
+                    nfi.NumberDecimalSeparator = ",";
+                    break;
+                default:
+                    Console.WriteLine("Unexpected number format selected in the dropdown.");
+                    break;
+            }
+
+            String exp_interest = Math.Round(amount * rate * term / 100 / year_length, 2).ToString("N", nfi);
+            
+            String act_interest = driver.FindElement(By.Id("interest")).GetAttribute("value");
+
+            //Assert
+            Assert.AreEqual(exp_interest, act_interest);
+        }
+
+
+        [Test]
+        public void Currency_Applied()
+        {
+            //Arrange
+            int currency_setting_item = 1; //[0; 2]
+
+            //Act
+            SelectElement currency_setting_select = new SelectElement(driver.FindElement(By.XPath("//tr[3]/td/select")));
+            currency_setting_select.SelectByIndex(currency_setting_item);
+
+            String selected_currency_setting = driver.FindElement(By.XPath("//tr[3]/td/select")).GetAttribute("value");
+
+            driver.FindElement(By.Id("save")).Click();
+            new WebDriverWait(driver, TimeSpan.FromMilliseconds(10000)).Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("amount")));
+
+            String exp_currency_sign = "";
+            switch (selected_currency_setting)
+            {
+                case "$ - US dollar":
+                    exp_currency_sign = "$";
+                    break;
+                case "€ - euro":
+                    exp_currency_sign = "€";
+                    break;
+                case "£ - Great Britain Pound":
+                    exp_currency_sign = "£";
+                    break;
+                default:
+                    Console.WriteLine("Unexpected currency selected in the dropdown.");
+                    break;
+            }
+
+            String act_currency_sign = driver.FindElement(By.XPath("//tr[1]/td[3]")).Text;
+
+            //Assert
+            Assert.AreEqual(exp_currency_sign, act_currency_sign);
         }
     }
 }
