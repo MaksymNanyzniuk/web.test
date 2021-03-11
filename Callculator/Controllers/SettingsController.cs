b7ad0123bcde38787819b4f0217c1a13882c2194
Sergii Dmytrenko <SEDO@simcorp.com>
2021-03-11 11:01:27 +0200

    apply settings

--- Callculator/Controllers/SettingsController.cs
+++ Callculator/Controllers/SettingsController.cs
@@ -1,12 +1,70 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Web.Mvc;
 
 namespace Callculator.Controllers
 {
-	public class SettingsController : Controller
-	{
-		public ActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class SettingsController : Controller
+    {
+        private readonly string settings = $"{Path.GetTempPath()}/settings.data";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Date(string date)
+        {
+            var format = System.IO.File.Exists(settings)
+                ? System.IO.File.ReadAllText(settings).Split(';')[0]
+                : "dd/MMM/yyyy";
+
+            return Json(DateTime.Parse(date).ToString(format, CultureInfo.InvariantCulture), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Number(float number)
+        {
+            var result = string.Format(CultureInfo.InvariantCulture, "{0:N}", number);
+            var format = System.IO.File.Exists(settings)
+                ? System.IO.File.ReadAllText(settings).Split(';')[1]
+                : "123,456,789.00";
+
+            switch (format)
+            {
+                case "123.456.789,00":
+                    result = result.Replace(',', ' ').Replace('.', ',').Replace(' ', '.');
+                    break;
+
+                case "123 456 789.00":
+                    result = result.Replace(',', ' ');
+                    break;
+
+                case "123 456 789,00":
+                    result = result.Replace(',', ' ').Replace('.', ',');
+                    break;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Currency()
+        {
+            var result = System.IO.File.Exists(settings)
+                ? System.IO.File.ReadAllText(settings).Split(';')[2]
+                : "$";
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Save(string dateFormat, string numberFormat, string currency)
+        {
+            System.IO.File.WriteAllText(settings, $"{dateFormat};{numberFormat};{currency.Split(' ')[0]}");
+            return Json("OK");
+        }
+    }
 }
